49ef1980a7fb15ec91930157d2e9fb80951e53b8
Merryn <merrynhr@gmail.com>
2021-05-25 15:28:03 +0100

    release bike rspec test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,14 @@
-describe "DockingStation" do 
-    it "returns bike when passed give me be bike" do
-    expect(DockingStation("give me be a bike")).to eq bike
-end
-end
+require 'docking_station'
+
+# describe DockingStation do 
+#     it "releases bike when asked" do
+#     expect(DockingStation).to respond_to(:release_bike)
+#     end    
+# end
+#     it "returns bike when passed give me be bike" do
+#     expect(DockingStation("give me be a bike")).to eq bike
+# end
+
+describe DockingStation do 
+    it { is_expected.to respond_to(:release_bike)}
+end    
